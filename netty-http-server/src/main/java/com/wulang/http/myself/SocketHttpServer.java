7f813f292c79fe74b9cf24378a3545f2fa1cca5b
wulang <1359208000@qq.com>
2018-07-26 15:35:31 +0800

    添加简单socket实现的http服务器

--- netty-http-server/src/main/java/com/wulang/http/myself/SocketHttpServer.java
+++ netty-http-server/src/main/java/com/wulang/http/myself/SocketHttpServer.java
@@ -1,11 +1,12 @@
 package com.wulang.http.myself;
 
-import java.io.OutputStream;
-import java.io.PrintStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
-public class SocketTest {
+public class SocketHttpServer {
+    private static int count=1;
     public static void main(String[] args) throws Exception {
         ServerSocket serverSocket=new ServerSocket(555);
 //        while(true){
@@ -22,16 +23,19 @@ public class SocketTest {
 //        }
         while(true){
             Socket socket = serverSocket.accept();
+            System.out.println(count++);
             OutputStream outputStream=socket.getOutputStream();
+            InputStream inputStream=socket.getInputStream();
+            byte b[]=new byte[1024];
+            inputStream.read(b);
+            System.out.println(new String(b));
+            PrintWriter printWriter=new PrintWriter(outputStream,true);
             String html="<!DOCTYPE html><html><head><title>浪哥</title></head><body><h4>浪哥流弊</h4></body></html>";
-            String response="HTTP/1.1 200 ok\r\n"+"content-type:text/html;charset=utf-8\r\n"+"\r\n"+"123";
-            outputStream.write(response.getBytes());
-            outputStream.flush();
-            outputStream.close();
+            String response="HTTP/1.1 200 ok\n"+"content-type:text/html;charset=utf-8\n"+"\n"+html;
+            printWriter.print(response);
+            printWriter.close();
             socket.close();
-            System.out.println("hi");
         }
-//        System.out.println("hi\\n1");
     }
 }
 
